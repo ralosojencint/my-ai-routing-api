b75ed41bc3d780e42831c48117c31e0a2f994e89
ralosojencint <ralosojencint047@gmail.com>
2026-08-12 08:52:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,11 +122,10 @@ def discover_gemini_model():
         return None
 
     preferred = [
-    "gemini-2.5-flash",
-    "gemini-2.5-flash-lite",
-    "gemini-3-flash-preview",
+    "gemini-3.5-flash",
     "gemini-3.1-flash-lite",
-]
+    "gemini-3.6-flash",
+    ]
 
     available = {}
 
